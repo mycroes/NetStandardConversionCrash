49f542de901cc59329d65b1d907bc6f779b9afb7
Michael Croes <mycroes@gmail.com>
2018-06-14 00:11:36 +0200

    Add testcase

---
+++ NetStandardConversionCrash/Test.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace NetStandardConversionCrash
+{
+    public class Test
+    {
+        public async Task A(params object[] items)
+        {
+            SetTo0(items, await B());
+        }
+
+        private async Task<int> B() => 0;
+
+        private void SetTo0(in IEnumerable<object> items, in int length)
+        {
+        }
+    }
+}
